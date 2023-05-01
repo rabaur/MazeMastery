ea2d3bcde74400a65c0bff336ce0fdc3049867b0
Raphaël Baur <rabaur@student.ethz.ch>
2023-05-01 09:29:59 +0200

    need to change pos to local for recursion to work

--- minosrecurse/maze_utils.py
+++ minosrecurse/maze_utils.py
@@ -1,5 +1,39 @@
+import random
 
 def get_maze_size(maze):
     m = max(i for i, _ in maze.keys()) + 1  # Number of rows
     n = max(j for _, j in maze.keys()) + 1  # Number of columns
     return m, n
+
+def create_corridor(length):
+    """
+    Generates a maze of length `length` that is a single path.
+    """
+    maze = {}
+    for j in range(1, length-1):
+        maze[(0, j)] = [(0, j+1), (0, j-1)]
+    maze[(0, 0)] = [(0, 1)]
+    maze[(0, length-1)] = [(0, length-2)]
+    return maze
+
+def create_SAW(rows, cols):
+    """
+    Generates a maze of size `rows` x `cols` that contains a self-avoiding walk.
+    """
+    path = []
+    pos = (0, 0)
+    offsets = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+    while True:
+        path.append(pos)
+        potential_neighbors = [(pos[0] + i, pos[1] + j) for i, j in offsets]
+        neighbors = [n for n in potential_neighbors if n[0] >= 0 and n[0] < rows and n[1] >= 0 and n[1] < cols]
+        neighbors = [n for n in neighbors if n not in path]
+        if len(neighbors) == 0:
+            break
+        pos = random.choice(neighbors)
+    maze = {(i, j): [] for i in range(rows) for j in range(cols)}
+    for i in range(1, len(path) - 1):
+        maze[path[i]] = [path[i-1], path[i+1]]
+    maze[path[0]] = [path[1]]
+    maze[path[-1]] = [path[-2]]
+    return maze, path
--- solution.py
+++ solution.py
@@ -1,28 +1,59 @@
 import tkinter as tk
 import random
 from minosrecurse.maze import create_maze
-from minosrecurse.renderer import draw_maze, draw_gems, draw_minotaurus, draw_player, draw_path_segment
+from minosrecurse.maze_utils import create_corridor, create_SAW
+from minosrecurse.renderer import (
+    draw_maze,
+    draw_gems,
+    draw_minotaurus,
+    draw_player,
+    draw_path_segment,
+)
 
-random.seed(1)
+LEVEL = 6
+random.seed(0)
 red_gem_coords = []
 blue_gem_coords = []
 found = False
 rows, cols = 15, 10
 cell_size = 50
-maze = create_maze(rows, cols, (0, 0), 1.0)
+
+if LEVEL == 1:
+    maze = create_corridor(cols)
+    minotaurus = (0, cols - 1)
+elif LEVEL == 2:
+    maze = create_corridor(cols)
+    minotaurus = (0, random.choice(range(1, cols - 1)))
+elif LEVEL == 3:
+    maze, path = create_SAW(rows, cols)
+    minotaurus = path[-1]
+elif LEVEL == 4:
+    maze = create_maze(rows, cols, (0, 0), 0.0)
+    minotaurus = (rows - 1, cols - 1)
+elif LEVEL == 5:
+    maze = create_maze(rows, cols, (0, 0), 1.0)
+    minotaurus = (rows - 1, cols - 1)
+elif LEVEL == 6:
+    maze = create_maze(rows, cols, (0, 0), 1.0)
+    minotaurus = (rows - 1, cols - 1)
+
+
 root = tk.Tk()
 root.title("Maze")
 canvas = tk.Canvas(root, width=cols * cell_size, height=rows * cell_size)
 canvas.pack()
-render_delay = 100
+render_delay = 250
 
 draw_maze(maze, canvas=canvas, cell_size=cell_size)
-minotaurus = random.choice(list(maze.keys()))
 draw_minotaurus(minotaurus, canvas, cell_size // 2, cell_size)
 pos = (0, 0)
 
+
 def move(new):
     global pos
+    if new not in maze[pos]:
+        print("OUCH!")
+        new = pos
     draw_path_segment(pos, new, canvas, cell_size)
     pos = new
     render(pos)
@@ -34,27 +65,33 @@ def render(pos):
     canvas.update()
     canvas.after(render_delay)
 
+
 def put_blue_gem(cell):
     if cell not in blue_gem_coords:
         blue_gem_coords.append(cell)
 
+
 def has_red_gem(cell):
     return cell in red_gem_coords
+
 
 def has_blue_gem(cell):
     return cell in blue_gem_coords
 
-def turn_gem_red(cell):
-    if cell in blue_gem_coords:
-        blue_gem_coords.remove(cell)
+
+def put_red_gem(cell):
+    if cell not in red_gem_coords:
         red_gem_coords.append(cell)
+
 
 def found_minotaurus():
     global found
     found = True
 
+
 def was_found():
     return found
+
 
 def get_neighbors(pos):
     """
@@ -65,25 +102,103 @@ def get_neighbors(pos):
 
 stack = []
 
-def DFS():
+
+def search1():
+    global pos
+    while True:
+        i, j = pos
+        neighbor = (i, j + 1)
+        move(neighbor)
+
+def search2():
+    global pos
+    while (not was_found()):
+        i, j = pos
+        neighbor = (i, j + 1)
+        move(neighbor)
+        if neighbor == minotaurus:
+            found_minotaurus()
+
+def search3():
     global pos
     while (not was_found()):
         put_blue_gem(pos)
-        if pos == minotaurus:
+        all_neighbors = get_neighbors(pos)
+        neighbors = []
+        for neighbor in all_neighbors:
+            if not has_blue_gem(neighbor):
+                neighbors.append(neighbor)
+        neighbor = neighbors[0]
+        move(neighbor)
+        if neighbor == minotaurus:
             found_minotaurus()
-            return
-        neighbors = get_neighbors(pos)
-        unvisited_neighbors = []
-        for neighbor in neighbors:
-            if not has_blue_gem(neighbor) and not has_red_gem(neighbor):
-                unvisited_neighbors.append(neighbor)
-        if unvisited_neighbors != []:
+
+def search4():
+    global pos
+    while (not was_found()):
+        put_blue_gem(pos)
+        all_neighbors = get_neighbors(pos)
+        neighbors = []
+        for neighbor in all_neighbors:
+            if not has_blue_gem(neighbor):
+                neighbors.append(neighbor)
+        if neighbors != []:
             stack.append(pos)
-            move(random.choice(unvisited_neighbors))
+            neighbor = neighbors[0]
         else:
-            turn_gem_red(pos)
-            move(stack.pop())
-DFS()
+            put_red_gem(pos)
+            neighbor = stack.pop()
+        move(neighbor)
+        if neighbor == minotaurus:
+            found_minotaurus()
+
+# Probably remove
+def search5():
+    global pos
+    while (not was_found()):
+        put_blue_gem(pos)
+        all_neighbors = get_neighbors(pos)
+        neighbors = []
+        for neighbor in all_neighbors:
+            if not has_blue_gem(neighbor):
+                neighbors.append(neighbor)
+        if neighbors != []:
+            stack.append(pos)
+            neighbor = neighbors[0]
+        else:
+            put_red_gem(pos)
+            neighbor = stack.pop()
+        move(neighbor)
+        if neighbor == minotaurus:
+            found_minotaurus()
+
+def search6():
+    if was_found():
+        return
+    put_blue_gem(pos)
+    all_neighbors = get_neighbors(pos)
+    for neighbor in all_neighbors:
+        if not has_blue_gem(neighbor) and not has_red_gem(neighbor):
+            if neighbor == minotaurus:
+                found_minotaurus()
+            move(neighbor)
+            search6()
+            put_red_gem(pos)
+
+
+if LEVEL == 1:
+    search1()
+elif LEVEL == 2:
+    search2()
+elif LEVEL == 3:
+    search3()
+elif LEVEL == 4:
+    search4()
+elif LEVEL == 5:
+    search5()
+elif LEVEL == 6:
+    search6()
+
 
 # Run the main loop
 root.mainloop()

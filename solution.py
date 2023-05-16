ea43eb0d5e49375791bef15641fa5986bcf9cce7
Donnerdistel <hartmannjs@icloud.com>
2023-05-16 19:37:22 +0200

    small changes to contain modular implementation

--- solution.py
+++ solution.py
@@ -3,12 +3,13 @@ from minosrecurse.maze import create_maze
 from minosrecurse.maze_utils import create_corridor, create_SAW
 from minosrecurse.renderer import Renderer
 
-LEVEL = 3
+
+LEVEL = int(input("Enter a level: "))
 random.seed(3)
 red_gem_coords = []
 blue_gem_coords = []
 found = False
-rows, cols = 15, 10
+rows, cols = 10, 10
 
 if LEVEL == 1:
     maze = create_corridor(cols)
@@ -151,22 +152,26 @@ def level5():
             if not has_blue_gem(neighbor):
                 neighbors.append(neighbor)
         if neighbors != []:
-            stack.append(pos)
             neighbor = neighbors[0]
+            stack.append(pos)
         else:
-            put_red_gem(pos)
+
             neighbor = stack.pop()
+            put_red_gem(pos)
         move(neighbor)
         if neighbor == minotaurus:
             found_minotaurus()
 
+
 def level6():
     put_blue_gem(pos)
     all_neighbors = get_neighbors(pos)
+
     for neighbor in all_neighbors:
         if not has_blue_gem(neighbor):
             old_pos = pos
             if was_found(): return
+
             move(neighbor)
             if neighbor == minotaurus:
                 found_minotaurus()

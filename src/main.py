96d82a7b610df967cbf4ff8385a32ecc65efc37d
Raphaël Baur <rabaur@student.ethz.ch>
2023-10-21 18:55:18 +0200

    removed redundant code in main

--- src/main.py
+++ src/main.py
@@ -6,15 +6,4 @@ from level4 import solve as level4
 from level5 import solve as level5
 from level6 import solve as level6
 
-
-def jumpy_recursion(node):
-    if not has_blue_gem(node):
-        set_pos(node)
-        put_blue_gem()
-        neighbors = get_neighbors()
-        for neighbor in neighbors:
-            if not has_blue_gem(neighbor):
-                jumpy_recursion(neighbor)
-
-
-run(4, lambda: jumpy_recursion((0, 0)), delay=100, cell_size=50, rows=7, cols=7, seed=2)
+run(1, level1, delay=100, cell_size=100, rows=7, cols=7, seed=2)

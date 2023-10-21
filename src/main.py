8769b2e4125bd0a51a57fee72e58a833bd8cc05c
awelski <rabaur@student.ethz.ch>
2023-10-21 14:35:36 +0200

    Remove test solution

--- src/main.py
+++ src/main.py
@@ -6,14 +6,4 @@ from level4 import solve as level4
 from level5 import solve as level5
 from level6 import solve as level6
 
-
-def test():
-    while not has_minotaur():
-        put_blue_gem()
-        for neighbor in get_neighbors():
-            if not has_blue_gem(neighbor):
-                new_pos = neighbor
-        set_pos(new_pos)
-
-
-run(3, test, delay=100, cell_size=50, rows=11, cols=11)
+run(3, level3, delay=100, cell_size=50, rows=11, cols=11)

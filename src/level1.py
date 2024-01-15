e298af4eaac26f52e66dd6ec399aa0ce2850db9d
Raphaël Baur <rabaur@student.ethz.ch>
2024-01-15 13:23:11 +0100

    deleted unused solution files

--- src/level1.py
+++
@@ -1,8 +0,0 @@
-from mazemastery.api import *
-
-
-def solve() -> None:
-    while True:
-        i, j = get_pos()
-        new_pos = (i, j + 1)
-        set_pos(new_pos)
--- src/level2.py
+++
@@ -1,8 +0,0 @@
-from mazemastery.api import *
-
-
-def solve() -> None:
-    while not has_minotaur():
-        i, j = get_pos()
-        new_pos = (i, j + 1)
-        set_pos(new_pos)
